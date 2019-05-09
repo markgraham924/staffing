980d99eacbf43fa15287d94eb08beffca08f027b
markgraham924 <50046031+markgraham924@users.noreply.github.com>
2019-05-09 20:33:40 +0100

    Using PHP to interact with SQL database.
    As this currently is not working i have switched to using Node.JS whilst I figure out how to solve the problem, or deciede that node is better.

--- submit.php
+++ submit.php
@@ -2,13 +2,29 @@
 $servername = "89.34.96.142";
 $username = "mcgnnpe_adminsql";
 $password = "mcgraham924";
+$dbname = "mcgnnpe_bylat"
 
 // Create connection
-$conn = mysqli_connect($servername, $username, $password);
-
+$conn = new mysqli($servername, $username, $password, $dbname);
 // Check connection
-if (!$conn) {
-    die("Connection failed: " . mysqli_connect_error());
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+} 
+
+// sql to create table
+$sql = "CREATE TABLE MyGuests (
+id INT(6) UNSIGNED AUTO_INCREMENT PRIMARY KEY, 
+firstname VARCHAR(30) NOT NULL,
+lastname VARCHAR(30) NOT NULL,
+email VARCHAR(50),
+reg_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
+)";
+
+if ($conn->query($sql) === TRUE) {
+    echo "Table MyGuests created successfully";
+} else {
+    echo "Error creating table: " . $conn->error;
 }
-echo "Connected successfully";
+
+$conn->close();
 ?>
